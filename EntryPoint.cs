38423ebd5c1f4dd22be99461a578fda384b7a61f
Lenny-del <lenny.kassel29@gmail.com>
2021-10-31 14:34:44 +0100

    First attempts of the ReloadSettings command

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -9,9 +9,21 @@ namespace TrunkPlugin
     {
         public static void Main()
         {
+            Game.AddConsoleCommands();
             Settings.LoadSettings();
             TrunkPlugin.Main.MainFiber();
             Game.LogTrivial("TrunkPlugin " + Assembly.GetExecutingAssembly().GetName().Version + " by Vielfalt has been initialised.");
+        }
+
+
+        public static class Commands
+        {
+            [Rage.Attributes.ConsoleCommand("This is a test.")]
+            public static void Test()
+            {
+                Game.LogTrivial("This is a test.");
+                Game.Console.Print("This is another test.");
+            }
         }
     }
 }
