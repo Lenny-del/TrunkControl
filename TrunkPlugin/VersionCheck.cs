d1f7640f4e18f66c843aedfdb73f2038bdc1695a
Lenny-del <lenny.kassel29@gmail.com>
2021-10-31 19:18:42 +0100

    Improved ver check

--- TrunkPlugin/VersionCheck.cs
+++ TrunkPlugin/VersionCheck.cs
@@ -9,24 +9,32 @@ namespace TrunkPlugin
         public static bool isUpdateAvailable()
         {
             string curVersion = Settings.CalloutVersion;
-            Uri latestVersionuri = new Uri("______");
+            Uri latestVersionuri = new Uri("https://github.com/Lenny-del/TrunkPlugin");
             WebClient client = new WebClient();
             string receivedData = string.Empty;
             try
             {
-                receivedData = client.DownloadString("______").Trim();
+                receivedData = client.DownloadString("https://github.com/Lenny-del/TrunkPlugin").Trim();
             }
             catch (WebException)
             {
                 Game.Console.Print();
+                Game.Console.Print("------------------------------------- TrunkPlugin -------------------------------------");
+                Game.Console.Print();
                 Game.Console.Print("[WARNING] Failed to check for an update.");
                 Game.Console.Print("[WARNING] Please make sure you are online or try to reload the plugin.");
+                Game.Console.Print();
+                Game.Console.Print("------------------------------------- TrunkPlugin -------------------------------------");
                 Game.Console.Print();
             }
             if (receivedData != Settings.CalloutVersion)
             {
                 Game.Console.Print();
+                Game.Console.Print("------------------------------------- TrunkPlugin -------------------------------------");
+                Game.Console.Print();
                 Game.Console.Print("[WARNING] A new version of TrunkPlugin is available. Please update to the latest build.");
+                Game.Console.Print();
+                Game.Console.Print("------------------------------------- TrunkPlugin -------------------------------------");
                 Game.Console.Print();
                 return true;
             }
