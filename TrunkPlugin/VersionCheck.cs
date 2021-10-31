4abf7d2d44cb8fe3d9bd15fed80cbd16a4269cdf
Lenny-del <lenny.kassel29@gmail.com>
2021-10-31 19:53:37 +0100

    Changed .txt to .xml in VersionChecker.cs

--- TrunkPlugin/VersionCheck.cs
+++ TrunkPlugin/VersionCheck.cs
@@ -9,12 +9,12 @@ namespace TrunkPlugin
         public static bool isUpdateAvailable()
         {
             string curVersion = Settings.CalloutVersion;
-            Uri latestVersionuri = new Uri("https://github.com/Lenny-del/TrunkPlugin/blob/master/TrunkPlugin/version.txt");
+            Uri latestVersionuri = new Uri("https://github.com/Lenny-del/TrunkPlugin/blob/master/TrunkPlugin/version.xml");
             WebClient client = new WebClient();
             string receivedData = string.Empty;
             try
             {
-                receivedData = client.DownloadString("https://github.com/Lenny-del/TrunkPlugin/blob/master/TrunkPlugin/version.txt").Trim();
+                receivedData = client.DownloadString("https://github.com/Lenny-del/TrunkPlugin/blob/master/TrunkPlugin/version.xml").Trim();
             }
             catch (WebException)
             {
