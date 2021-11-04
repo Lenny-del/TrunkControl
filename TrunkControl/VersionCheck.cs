c32e52e27b6b1358b2826dab7db56637802c6e03
Lenny <70054724+Lenny-del@users.noreply.github.com>
2021-11-04 15:22:56 +0100

    test

--- TrunkControl/VersionCheck.cs
+++ TrunkControl/VersionCheck.cs
@@ -19,12 +19,12 @@ namespace TrunkControl
             catch (WebException)
             {
                 Game.Console.Print();
-                Game.Console.Print("------------------------------------- TrunkControl -------------------------------------");
+                Game.Console.Print("------------------------------------- TrunkControl [WARNING] -------------------------------------");
                 Game.Console.Print();
                 Game.Console.Print("[WARNING] Failed to check for an update.");
                 Game.Console.Print("[WARNING] Please make sure you are online or try to reload the plugin.");
                 Game.Console.Print();
-                Game.Console.Print("------------------------------------- TrunkControl -------------------------------------");
+                Game.Console.Print("------------------------------------- TrunkControl [WARNING] -------------------------------------");
                 Game.Console.Print();
                 Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "TrunkControl", "~y~v" + Settings.PluginVersion + " ~o~by Vielfalt", "~r~Couldn't check for latest version! <br>~w~Please make sure you are ~y~connected ~w~to the internet or ~y~reload ~w~the plugin.");
                 return false;
@@ -32,13 +32,13 @@ namespace TrunkControl
             if (receivedData != Settings.PluginVersion)
             {
                 Game.Console.Print();
-                Game.Console.Print("------------------------------------- TrunkControl -------------------------------------");
+                Game.Console.Print("------------------------------------- TrunkControl [WARNING] -------------------------------------");
                 Game.Console.Print();
                 Game.Console.Print("[WARNING] A new version of TrunkControl is available. Please update to the latest build.");
                 Game.Console.Print("[WARNING] Current version:  " + curVersion);
                 Game.Console.Print("[WARNING] New version:  " + receivedData);
                 Game.Console.Print();
-                Game.Console.Print("------------------------------------- TrunkControl -------------------------------------");
+                Game.Console.Print("------------------------------------- TrunkControl [WARNING] -------------------------------------");
                 Game.Console.Print();
                 Game.DisplayNotification("commonmenu", "mp_alerttriangle", "~w~TrunkControl Warning", "~y~A new Update is available!", "Current Version: ~r~" + curVersion + "~w~<br>New Version: ~g~" + receivedData + "<br>~r~Please update to the latest version!");
                 return true;
