d7288e60155064847294de6e710e810ddb51b7c6
Lenny <70054724+Lenny-del@users.noreply.github.com>
2021-12-02 16:04:44 +0100

    .

--- TrunkControl/Settings.cs
+++ TrunkControl/Settings.cs
@@ -6,9 +6,9 @@ namespace TrunkControl
     internal static class Settings
     {
         internal static Keys TrunkKey = Keys.T;
-        //internal static Keys MenuKey = Keys.F5;
+        internal static Keys MenuKey = Keys.F5;
         internal static ControllerButtons TrunkButton = ControllerButtons.None;
-        //internal static ControllerButtons MenuButton = ControllerButtons.None;
+        internal static ControllerButtons MenuButton = ControllerButtons.None;
         internal static readonly string PluginVersion = "1.0";
 
         internal static void LoadSettings()
@@ -22,8 +22,8 @@ namespace TrunkControl
             ini.Create();
             TrunkKey = ini.ReadEnum("Keys", "TrunkKey", Keys.T);
             TrunkButton = ini.ReadEnum<ControllerButtons>("Controller", "TrunkButton", ControllerButtons.None);
-            //MenuKey = ini.ReadEnum("Keys", "MenuKey", Keys.F5);
-            //MenuButton = ini.ReadEnum<ControllerButtons>("Controller", "MenuButton", ControllerButtons.None);
+            MenuKey = ini.ReadEnum("Keys", "MenuKey", Keys.F5);
+            MenuButton = ini.ReadEnum<ControllerButtons>("Controller", "MenuButton", ControllerButtons.None);
             Game.Console.Print("TrunkControl Config loaded.");
         }
     }
