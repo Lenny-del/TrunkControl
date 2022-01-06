877f4025fdae2fc57594ef483a48458a2fed78a9
Lenny <70054724+Lenny-del@users.noreply.github.com>
2022-01-06 19:07:19 +0100

    removed version checker stuff from EntryPoint.cs

--- TrunkControl/EntryPoint.cs
+++ TrunkControl/EntryPoint.cs
@@ -10,10 +10,6 @@ namespace TrunkControl
     {
         public static void Main()
         {
-            string curVersion = Settings.PluginVersion;
-            Uri latestVersionuri = new Uri("https://www.lcpdfr.com/applications/downloadsng/interface/api.php?do=checkForUpdates&fileId=36974&textOnly=1");
-            WebClient client = new WebClient();
-            string receivedData = string.Empty;
             Game.AddConsoleCommands();
             Settings.LoadSettings();
             TrunkControl.Main.MainFiber();
